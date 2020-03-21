61ea2399cb83696c8f09c3b6d538be0c94303040
prashant singh <prashantsinghmangat5@gmail.com>
2020-03-22 01:33:28 +0530

    mhmsbmrapp

--- app/src/main/java/com/example/mhmsbmrapp/DashboardBmr/Out_Patient_Dashboard/activity/OpTherapy/OpTherapyHistory.java
+++ app/src/main/java/com/example/mhmsbmrapp/DashboardBmr/Out_Patient_Dashboard/activity/OpTherapy/OpTherapyHistory.java
@@ -1,23 +1,122 @@
 package com.example.mhmsbmrapp.DashboardBmr.Out_Patient_Dashboard.activity.OpTherapy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.mhmsbmrapp.AddPatientFolder.AddPatients;
+import com.example.mhmsbmrapp.DashboardBmr.Out_Patient_Dashboard.adapter.RecyclerViewAdapter;
+import com.example.mhmsbmrapp.DashboardBmr.Out_Patient_Dashboard.model.Anime;
 import com.example.mhmsbmrapp.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OpTherapyHistory extends Fragment {
-    private static final String TAG = "Fragment2";
+    private static final String TAG = "Fragment3";
+
+    Button button;
+    TextView addpatienttext;
+
+    private final String JSON_URL = "https://gist.githubusercontent.com/aws1994/f583d54e5af8e56173492d3f60dd5ebf/raw/c7796ba51d5a0d37fc756cf0fd14e54434c547bc/anime.json" ;
+    private JsonArrayRequest request ;
+    private RequestQueue requestQueue ;
+    private List<Anime> lstAnime ;
+    private RecyclerView recyclerView ;
+
+    public OpTherapyHistory() {
+        // Required empty public constructor
+    }
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
-        View view  = inflater.inflate(R.layout.op_therapy_history, container, false);
+        View v = inflater.inflate(R.layout.op_therapy_history, container, false);
 
-        return view;
+
+
+        lstAnime = new ArrayList<>() ;
+        recyclerView = v.findViewById(R.id.recyclerviewid);
+        jsonrequest();
+
+        //json//
+        return v;
+    }
+
+
+    private void jsonrequest() {
+
+        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                JSONObject jsonObject  = null ;
+
+                for (int i = 0 ; i < response.length(); i++ ) {
+
+
+                    try {
+                        jsonObject = response.getJSONObject(i) ;
+                        Anime anime = new Anime() ;
+                        anime.setName(jsonObject.getString("name"));
+                        anime.setDescription(jsonObject.getString("description"));
+                        anime.setRating(jsonObject.getString("Rating"));
+                        anime.setCategorie(jsonObject.getString("categorie"));
+                        anime.setNb_episode(jsonObject.getInt("episode"));
+                        anime.setStudio(jsonObject.getString("studio"));
+                        anime.setImage_url(jsonObject.getString("img"));
+                        lstAnime.add(anime);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+
+                setuprecyclerview(lstAnime);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.add(request) ;
+
+
+    }
+
+    private void setuprecyclerview(List<Anime> lstAnime) {
+
+
+        RecyclerViewAdapter myadapter = new RecyclerViewAdapter(getActivity(),lstAnime) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(myadapter);
+
     }
 }
-
